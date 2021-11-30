6bee32d51e34fca77cb6e36f18ca9a3613ac66e6
Demner <rlopezcr@everis.com>
2021-11-29 21:33:06 -0500

    script para crear proyectos maven

---
+++ create-mvn-project.sh
@@ -0,0 +1,6 @@
+mvn archetype:generate \
+     -DgroupId=com.dmnr.aws \
+     -DartifactId=dmnr-greetings \
+     -DarchetypeArtifactId=maven-archetype-quickstart \
+     -DarchetypeVersion=1.4 \
+     -DinteractiveMode=false
